be2cdbbd4579a8f80a0637e0c4deba169b13d9bc
Hem Gajjar <hemgajjaris007@gmail.com>
2025-07-16 18:49:11 +0530

    Add sweet route created

---
+++ backend/routes/sweetRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+    addSweet,
+    getSweets,
+    deleteSweet,
+    purchaseSweet,
+    restockSweet
+} = require('../controllers/sweetController');
+
+router.post('/', addSweet);
+
+
+module.exports = router;
